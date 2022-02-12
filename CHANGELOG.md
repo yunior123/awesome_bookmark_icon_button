9c200ea130435aa9dd9bf1ae6136e58acb4623de
yunior123 <yuniorrodriguezo460@gmail.com>
2022-02-11 21:01:55 -0500

    some minor improvements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1
 
 - Creating the package
+
+## 0.0.2
+
+- Adding more parameters to inner Icon Button
--- lib/awesome_bookmark_icon_button.dart
+++ lib/awesome_bookmark_icon_button.dart
@@ -7,10 +7,17 @@ class BookMarkIconButton extends StatefulWidget {
     Key? key,
     required this.onPressed,
     required this.isSaved,
+    this.constraints,
+    this.iconSize,
+    required this.padding,
   }) : super(key: key);
 
   final VoidCallback onPressed;
   final bool isSaved;
+  final BoxConstraints? constraints;
+  final double? iconSize;
+  final EdgeInsetsGeometry padding;
+
   @override
   State<BookMarkIconButton> createState() => _BookMarkIconButtonState();
 }
@@ -27,6 +34,9 @@ class _BookMarkIconButtonState extends State<BookMarkIconButton> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
+      constraints: widget.constraints,
+      iconSize: widget.iconSize,
+      padding: widget.padding,
       onPressed: () {
         setState(
           () {
